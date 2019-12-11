56a6b63a52ae1da33953461e63d528153a436ca0
Max Proskauer <mprosk@gmail.com>
2019-12-10 22:48:36 -0500

    use valid flag; cleanup

--- gamecube_midi.ino
+++ gamecube_midi.ino
@@ -50,7 +50,6 @@ void gc_low(uint8_t pin);
 void gc_high(uint8_t pin);
 void gc_stop_bit(uint8_t pin);
 void gc_get_status(GameCube* gc);
-void gc_parse_status(void);
 uint8_t gc_slice_byte(uint8_t b_start);
 void gc_print_status(GameCube* gc);
 
@@ -83,6 +82,9 @@ void gc_stop_bit(uint8_t pin)
 // Send the 24-'byte' sequence to request the controller's status
 void gc_get_status(GameCube* gc)
 { 
+  unsigned long count = 0;
+  gc_data_pointer = 0;
+  gc->valid = 1;
   noInterrupts();   // Disable interrupts to prevent distractions during time-critical operation
   
   // PART 1: Send status request to controller
@@ -96,15 +98,17 @@ void gc_get_status(GameCube* gc)
   gc_stop_bit(gc->tx_pin);                                       // stop bit
 
   // PART 2: Retrieve status report from controller
-  gc_data_pointer = 0;
-  unsigned long count = 0;
   for(uint8_t i = 0; i < 64; i++)
   {
     count = 0;
     while(digitalReadFast(gc->rx_pin) == 1)   // wait for pin to go low
     {
       count++;
-      if(count > 10000) {break;}
+      if(count > 10000)
+      {
+        gc->valid = 0;
+        break;
+      }
     };
     delayMicroseconds(2);   // adjust as needed
     digitalWriteFast(ISR_STATUS_PIN, HIGH);
@@ -115,32 +119,38 @@ void gc_get_status(GameCube* gc)
     while(digitalReadFast(gc->rx_pin) == 0)   // wait for pin to go high
     {
       count++;
-      if(count > 10000) {break;}
+      if(count > 10000)
+      {
+        gc->valid = 0;
+        break;
+      }
     };  
-    
   }
   interrupts();   // Be sure to re-enable interrupts or the USB stuff breaks
 
   // PART 3: Parse status into data structure
-  gc->btn_start   = gc_data_buffer[3];
-  gc->btn_y       = gc_data_buffer[4];
-  gc->btn_x       = gc_data_buffer[5];
-  gc->btn_b       = gc_data_buffer[6];
-  gc->btn_a       = gc_data_buffer[7];
-  gc->btn_lt      = gc_data_buffer[9];
-  gc->btn_rt      = gc_data_buffer[10];
-  gc->btn_z       = gc_data_buffer[11];
-  gc->btn_d_up    = gc_data_buffer[12];
-  gc->btn_d_down  = gc_data_buffer[13];
-  gc->btn_d_right = gc_data_buffer[14];
-  gc->btn_d_left  = gc_data_buffer[15];
-
-  gc->joy_x    = gc_slice_byte(2*8) & (~0x01);
-  gc->joy_y    = gc_slice_byte(3*8) & (~0x01);
-  gc->cstick_x = gc_slice_byte(4*8) & (~0x01);
-  gc->cstick_y = gc_slice_byte(5*8) & (~0x01);
-  gc->ltrig    = gc_slice_byte(6*8) & (~0x01);
-  gc->rtrig    = gc_slice_byte(7*8) & (~0x01);
+  if(gc->valid)
+  {
+    gc->btn_start   = gc_data_buffer[3];
+    gc->btn_y       = gc_data_buffer[4];
+    gc->btn_x       = gc_data_buffer[5];
+    gc->btn_b       = gc_data_buffer[6];
+    gc->btn_a       = gc_data_buffer[7];
+    gc->btn_lt      = gc_data_buffer[9];
+    gc->btn_rt      = gc_data_buffer[10];
+    gc->btn_z       = gc_data_buffer[11];
+    gc->btn_d_up    = gc_data_buffer[12];
+    gc->btn_d_down  = gc_data_buffer[13];
+    gc->btn_d_right = gc_data_buffer[14];
+    gc->btn_d_left  = gc_data_buffer[15];
+  
+    gc->joy_x    = gc_slice_byte(2*8) & (~0x01);
+    gc->joy_y    = gc_slice_byte(3*8) & (~0x01);
+    gc->cstick_x = gc_slice_byte(4*8) & (~0x01);
+    gc->cstick_y = gc_slice_byte(5*8) & (~0x01);
+    gc->ltrig    = gc_slice_byte(6*8) & (~0x01);
+    gc->rtrig    = gc_slice_byte(7*8) & (~0x01);
+  }
 }
 
 uint8_t gc_slice_byte(uint8_t b_start)
@@ -153,26 +163,13 @@ uint8_t gc_slice_byte(uint8_t b_start)
   return out;
 }
 
-void gc_print_slice(uint8_t b_start)
-{
-  for(uint8_t i = b_start; i < b_start + 8; i++)
-  {
-    Serial.print(gc_data_buffer[i]);
-  }
-  Serial.println();
-}
-
-void gc_isr(void)
-{
-  noInterrupts();
-
-}
-
 ////////////////////////////////////////////////////////////////////////////////
 // GAMECUBE CONTROLLER PUBLIC FUNCTIONS
 
 void gc_print_status(GameCube* gc)
 {
+  Serial.print("ID: ");     Serial.println(gc->id);
+  Serial.print("VALID: ");  Serial.println(gc->valid);
   Serial.print("ST: ");     Serial.println(gc->btn_start);
   Serial.print("Y:  ");     Serial.println(gc->btn_y);
   Serial.print("X:  ");     Serial.println(gc->btn_x);
