19693410e54f9876e551a66ac0f5bec2dd0a456a
Max Proskauer <mprosk@gmail.com>
2020-01-01 22:10:48 -0500

    tweak functionality of second controller mapping

--- gamecube_midi.ino
+++ gamecube_midi.ino
@@ -9,10 +9,19 @@
 
 
 #define GC_CONTROLLER_COUNT   (2)
-#define MIDI_CHANNEL               (1)
+#define GC_POLLING_RATE_MS    (3)
+#define GC_MIDI_CHANNEL       (1)
+#define GC_LOWER_THRESH       (85)
+#define GC_UPPER_THRESH       (255 - GC_LOWER_THRESH)
 
 
-bool parse_action(uint8_t action, uint8_t param, uint8_t value)
+static int8_t gc_acc = 0;
+static int8_t gc_acc_digital = 0;
+static int8_t gc_acc_analog[] = {0, 0, 0, 0, 0, 0};
+static int8_t gc_analog_latch[] = {0, 0, 0, 0, 0, 0};
+
+
+bool parse_digital_action(int action, int param1, int param2, uint8_t value)
 {
   bool ret = true;
   switch (action)
@@ -20,20 +29,45 @@ bool parse_action(uint8_t action, uint8_t param, uint8_t value)
     case ACTION_NOTE:
       if(value)
       {
-        usbMIDI.sendNoteOn(param, 100, MIDI_CHANNEL);
+        usbMIDI.sendNoteOn(param1 + gc_acc, 100, GC_MIDI_CHANNEL);
       }
       else
       {
-        usbMIDI.sendNoteOff(param, 127, MIDI_CHANNEL);
+        usbMIDI.sendNoteOff(param1 + gc_acc, 127, GC_MIDI_CHANNEL);
       }
       break;
 
     case ACTION_CC:
-      usbMIDI.sendControlChange(param, value, MIDI_CHANNEL);
+      usbMIDI.sendControlChange(param1, value, GC_MIDI_CHANNEL);
       break;
 
     case ACTION_PANIC:
-      usbMIDI.sendControlChange(123, 0, MIDI_CHANNEL);
+      if(value)
+      {
+        usbMIDI.sendControlChange(123, 0, GC_MIDI_CHANNEL);
+      }
+      break;
+
+    case ACTION_KEY:
+      if(value)
+      {
+        Keyboard.press(KEY_ENTER);
+      }
+      else
+      {
+        Keyboard.release(KEY_ENTER);
+      }
+      break;
+
+    case ACTION_ACC:
+      if(value)
+      {
+        gc_acc_digital += param1;
+      }
+      else
+      {
+        gc_acc_digital -= param1;  
+      }
       break;
     
     default:
@@ -43,6 +77,74 @@ bool parse_action(uint8_t action, uint8_t param, uint8_t value)
   return ret;
 }
 
+
+bool parse_analog_action(uint8_t ctrl_idx, int action, int param1, int param2, uint8_t value)
+{
+  bool ret = true;
+  switch (action)
+  {
+    case ACTION_NOTE:
+      if(value <= GC_LOWER_THRESH)
+      {
+        if(gc_analog_latch[ctrl_idx] == 0)
+        {
+          gc_analog_latch[ctrl_idx] = 1;
+          usbMIDI.sendNoteOn(param1 + gc_acc, 100, GC_MIDI_CHANNEL);
+        }
+      }
+      else if(value >= GC_UPPER_THRESH)
+      {
+        if(gc_analog_latch[ctrl_idx] == 0)
+        {
+          gc_analog_latch[ctrl_idx] = 1;
+          usbMIDI.sendNoteOn(param2 + gc_acc, 100, GC_MIDI_CHANNEL);
+        }
+      }
+      else
+      {
+        if(gc_analog_latch[ctrl_idx] == 1)
+        {
+          gc_analog_latch[ctrl_idx] = 0;
+          usbMIDI.sendNoteOff(param1 + gc_acc, 127, GC_MIDI_CHANNEL);
+          usbMIDI.sendNoteOff(param2 + gc_acc, 127, GC_MIDI_CHANNEL);
+        }
+      }
+      break;
+
+    case ACTION_ACC:
+      if(value <= GC_LOWER_THRESH)
+      {
+        gc_acc_analog[ctrl_idx] = param1;
+      }
+      else if(value >= GC_UPPER_THRESH)
+      {
+        gc_acc_analog[ctrl_idx] = param2;
+      }
+      else
+      {
+        gc_acc_analog[ctrl_idx] = 0;
+      }
+      break;
+
+    case ACTION_CC:
+      usbMIDI.sendControlChange(param1, value, GC_MIDI_CHANNEL);
+      break;
+    
+    default:
+      ret = false;
+      break;
+  }
+  return ret;
+}
+
+void update_acc(void)
+{
+  gc_acc = gc_acc_digital;
+  for(uint8_t i = 0; i < GC_ANALOG_COUNT; i++)
+  {
+    gc_acc += gc_acc_analog[i];
+  }
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 // ARDUINO HOOKS
@@ -71,9 +173,9 @@ void loop(void)
   gc1.rx_pin = GC1_RX;
 
   GameCube gc1_last;
-  gc1.id = 1;
-  gc1.tx_pin = GC1_TX;
-  gc1.rx_pin = GC1_RX;
+  gc1_last.id = 1;
+  gc1_last.tx_pin = GC1_TX;
+  gc1_last.rx_pin = GC1_RX;
 
   GameCube gc2;
   gc2.id = 2;
@@ -81,9 +183,9 @@ void loop(void)
   gc2.rx_pin = GC2_RX;
 
   GameCube gc2_last;
-  gc2.id = 2;
-  gc2.tx_pin = GC2_TX;
-  gc2.rx_pin = GC2_RX;
+  gc2_last.id = 2;
+  gc2_last.tx_pin = GC2_TX;
+  gc2_last.rx_pin = GC2_RX;
 
   GameCube* gc_pointers[] = {&gc1, &gc2}; 
   GameCube* gc_last_pointers[] = {&gc1_last, &gc2_last};
@@ -96,7 +198,7 @@ void loop(void)
       GameCube* gc_ptr_last = gc_last_pointers[controller];
 
       gc_get_status(gc_ptr);
-      gc_print_status(gc_ptr);
+      //gc_print_status(gc_ptr);
 
       if(!gc_ptr->valid)
       {
@@ -108,12 +210,14 @@ void loop(void)
       {
         if( gc_ptr->buttons[i] != gc_ptr_last->buttons[i] )
         {
-          uint8_t action = action_mapping[controller][i][0];
-          uint8_t param =  action_mapping[controller][i][1];
+          int8_t action = action_mapping[controller][i][0];
+          int8_t param1 =  action_mapping[controller][i][1];
+          int8_t param2 =  action_mapping[controller][i][2];
 
-          if ( parse_action(action, param, gc_ptr->buttons[i]) )
+          if ( parse_digital_action(action, param1, param2, gc_ptr->buttons[i]) )
           {
-            digitalWriteFast(LED_PIN, HIGH);    
+            digitalWriteFast(LED_PIN, HIGH);
+            update_acc();
           }
           
         }
@@ -125,12 +229,14 @@ void loop(void)
       {
         if( gc_ptr->analog[i] != gc_ptr_last->analog[i] )
         {
-          uint8_t action = action_mapping[controller][i + GC_BUTTON_COUNT][0];
-          uint8_t param =  action_mapping[controller][i + GC_BUTTON_COUNT][1];
+          int8_t action = action_mapping[controller][i + GC_BUTTON_COUNT][0];
+          int8_t param1 =  action_mapping[controller][i + GC_BUTTON_COUNT][1];
+          int8_t param2 =  action_mapping[controller][i + GC_BUTTON_COUNT][2];
 
-          if ( parse_action(action, param, gc_ptr->analog[i]) )
+          if ( parse_analog_action(i, action, param1, param2, gc_ptr->analog[i]) )
           {
-           digitalWriteFast(LED_PIN, HIGH); 
+           digitalWriteFast(LED_PIN, HIGH);
+           update_acc();
           }
         }
         gc_ptr_last->analog[i] = gc_ptr->analog[i];
@@ -139,9 +245,9 @@ void loop(void)
 
     //usbMIDI.send_now();
 
+    delay(GC_POLLING_RATE_MS);
+     
     digitalWriteFast(LED_PIN, LOW);
-    
-    delay(6);
 
     // MIDI Controllers should discard incoming MIDI messages.
     while (usbMIDI.read()) {}
--- mapping.h
+++ mapping.h
@@ -9,87 +9,89 @@
 #define ACTION_CC         (2)
 #define ACTION_PITCH_BEND (3)
 #define ACTION_PANIC      (4)
+#define ACTION_KEY        (5)
+#define ACTION_ACC        (6)
 
 
-static const uint8_t action_mapping[][18][2] = {
+static const int action_mapping[][18][3] = {
   // Controller 1
   {
     // Start
-    {ACTION_PANIC, 0},      // Panic (all notes off)
+    {ACTION_KEY, KEY_ENTER, 0},      // enter key on keyboard
     // Y
-    {ACTION_NOTE, 49},
+    {ACTION_NOTE, 49, 0},
     // X
-    {ACTION_NOTE, 59},
+    {ACTION_NOTE, 59, 0},
     // B
-    {ACTION_NOTE, 44},
+    {ACTION_NOTE, 44, 0},
     // A
-    {ACTION_NOTE, 40},
+    {ACTION_NOTE, 40, 0},
     // L
-    {ACTION_NOTE, 39},
+    {ACTION_NOTE, 39, 0},
     // R
-    {ACTION_NOTE, 45},
+    {ACTION_NOTE, 45, 0},
     // Z
-    {ACTION_NOTE, 54},
+    {ACTION_NOTE, 54, 0},
     // D-Up
-    {ACTION_NOTE, 48},
+    {ACTION_NOTE, 48, 0},
     // D-Down
-    {ACTION_NOTE, 36},
+    {ACTION_NOTE, 36, 0},
     // D-Right
-    {ACTION_NOTE, 38},
+    {ACTION_NOTE, 38, 0},
     // D-Left
-    {ACTION_NOTE, 43},
+    {ACTION_NOTE, 43, 0},
     // Joystick X
-  	{ACTION_CC, 0},
+  	{ACTION_CC, 0, 0},
   	// Joystick Y
-  	{ACTION_CC, 1},
+  	{ACTION_CC, 1, 0},
   	// C-Stick X
-  	{ACTION_CC, 2},
+  	{ACTION_CC, 2, 0},
   	// C-Stick Y
-  	{ACTION_CC, 3},
+  	{ACTION_CC, 3, 0},
   	// Left Trigger
-  	{ACTION_CC, 4},
+  	{ACTION_CC, 4, 0},
   	// Right Trigger
-  	{ACTION_CC, 5}
+  	{ACTION_CC, 5, 0}
   },
   
   // Controller 2
   {
     // Start
-    {ACTION_NOP, 0},
+    {ACTION_PANIC, 0, 0},      // All notes off
     // Y
-    {ACTION_NOP, 0},
+    {ACTION_NOTE, 85, 0},
     // X
-    {ACTION_NOP, 0},
+    {ACTION_NOTE, 83, 0},
     // B
-    {ACTION_NOP, 0},
+    {ACTION_NOTE, 82, 0},
     // A
-    {ACTION_NOP, 0},
+    {ACTION_NOTE, 74, 0},
     // L
-    {ACTION_NOP, 0},
+    {ACTION_NOTE, 90, 0},
     // R
-    {ACTION_NOP, 0},
+    {ACTION_ACC, 1, 0},    // All notes +1
     // Z
-    {ACTION_NOP, 0},
+    {ACTION_ACC, -1, 0},    // All notes -1
     // D-Up
-    {ACTION_NOP, 0},
+    {ACTION_NOTE, 78, 0},
     // D-Down
-    {ACTION_NOP, 0},
+    {ACTION_NOTE, 73, 0},
     // D-Right
-    {ACTION_NOP, 0},
+    {ACTION_NOTE, 80, 0},
     // D-Left
-    {ACTION_NOP, 0},
+    {ACTION_NOTE, 75, 0},
     // Joystick X
-	  {ACTION_NOP, 0},
-  	// Joystick Y
-  	{ACTION_NOP, 0},
-  	// C-Stick X
-  	{ACTION_NOP, 0},
-  	// C-Stick Y
-  	{ACTION_NOP, 0},
-  	// Left Trigger
-  	{ACTION_NOP, 0},
-  	// Right Trigger
-  	{ACTION_NOP, 0}
+    {ACTION_NOP, 0, 0},
+    // Joystick Y
+    {ACTION_ACC, -2, 2},      // UP = All notes +2, DOWN = All notes -2
+    // C-Stick X
+    {ACTION_NOTE, 83, 81},    // LEFT = 83, RIGHT = 81
+    // C-Stick Y
+    {ACTION_NOTE, 77, 86},    // UP = 86, DOWN = 77
+    // Left Trigger
+    {ACTION_NOP, 0, 0},
+    // Right Trigger
+    {ACTION_NOP, 0, 0}
   }
 };
 
